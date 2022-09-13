8fd96408b326b189092dc00113fe78f6c9f02c24
RDDcat <yukmaro@gmail.com>
2022-09-13 22:01:42 +0900

    [UPDATE] 삽입정렬 구현 및 테스트 성공

--- src/com/company/InsertionSort.java
+++ src/com/company/InsertionSort.java
@@ -1,6 +1,23 @@
 package com.company;
 
-public class InsertionSort {
+import java.util.ArrayList;
+import java.util.List;
 
-
+public class InsertionSort implements Sort{
+    // 삽입정렬
+    @Override
+    public int[] Sort(int[] array) {
+        int n = array.length;
+        int j, key;
+        for (int i = 1; i < n; i++) {
+            key = array[i];
+            j = i-1;
+            while((j > -1) && (array[j] > key)){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+        }
+        return array;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] arr = {3,4,2,9,5,4,2,1,6};
+        PrintArray(arr);
+
+        Sort sort = new InsertionSort();
+        sort.Sort(arr);
+
+        PrintArray(arr);
+    }
+
+    // 인트 배열 출력
+    public static void PrintArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.println();
     }
 }
