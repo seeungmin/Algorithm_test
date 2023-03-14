96ac1432f7447b79df2516b86dfb2b4a412751d5
RDDcat <yukmaro@gmail.com>
2023-03-14 19:06:28 +0900

    [UPDATE] 문제 풀이 포멧 추가

---
+++ coding/src/main/java/com/maro/coding/leetcode/BinarySearch704.java
@@ -0,0 +1,6 @@
+package com.maro.coding.leetcode;
+
+public class BinarySearch704 {
+    // https://leetcode.com/problems/binary-search/?envType=study-plan&id=algorithm-i
+
+}
---
+++ coding/src/test/java/com/maro/coding/leetcode/BinarySearch704Test.java
@@ -0,0 +1,13 @@
+package com.maro.coding.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class BinarySearch704Test {
+    @Test
+    void example_1() {
+    }
+
+    @Test
+    void example_2() {
+    }
+}
