25e11f102f6285c2973087b559517d21101a1861
yukmaro <2018158018@tukorea.ac.kr>
2023-03-17 09:19:30 +0900

    [UPDATE] binary search update

--- coding/src/main/java/com/maro/coding/leetcode/BinarySearch704.java
+++ coding/src/main/java/com/maro/coding/leetcode/BinarySearch704.java
@@ -1,6 +1,28 @@
 package com.maro.coding.leetcode;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class BinarySearch704 {
     // https://leetcode.com/problems/binary-search/?envType=study-plan&id=algorithm-i
+    public int binarySearch(int[] nums, int target){
+        int halfLength = nums.length / 2;
 
+        if(target == nums[halfLength]) {
+            return halfLength;
+        }
+        return recursiveSearch(nums, target, halfLength);
+    }
+    public int recursiveSearch(int[] nums, int target, int halfLength){
+
+        if(target > nums[halfLength]){
+            if(target== nums[halfLength + (halfLength / 2)]) return halfLength + (halfLength / 2);
+            return recursiveSearch( nums, target,halfLength/2);
+        } else {
+            if(target== nums[halfLength - (halfLength / 2)]) return halfLength - (halfLength / 2);
+            return recursiveSearch( nums, target,halfLength/2);
+        }
+    }
 }
--- coding/src/test/java/com/maro/coding/leetcode/BinarySearch704Test.java
+++ coding/src/test/java/com/maro/coding/leetcode/BinarySearch704Test.java
@@ -1,13 +1,33 @@
 package com.maro.coding.leetcode;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinarySearch704Test {
+    private BinarySearch704 binarySearch = new BinarySearch704();
+
     @Test
+    @DisplayName("제공된 예시1")
     void example_1() {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+
+        int output = binarySearch.binarySearch(nums, target);
+
+        assertEquals(4, output);
     }
 
     @Test
+    @DisplayName("예시2 리스트에 값이 없으면 -1")
     void example_2() {
+        // nums = [-1,0,3,5,9,12], target = 2
+
+        int output = 0;
+
+
+        assertEquals(-1, output);
     }
 }
