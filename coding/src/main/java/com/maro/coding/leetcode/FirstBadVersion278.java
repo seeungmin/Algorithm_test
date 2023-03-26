fc5326018cc6cc2c9cf17eda0fedb327d9bc5305
Lee <lsmin476@naver.com>
2023-03-27 01:54:11 +0900

    |UPDATE| Algorithm 278 update 0327

--- coding/src/main/java/com/maro/coding/leetcode/FirstBadVersion278.java
+++ coding/src/main/java/com/maro/coding/leetcode/FirstBadVersion278.java
@@ -3,9 +3,19 @@ package com.maro.coding.leetcode;
 public class FirstBadVersion278 extends VersionControl {
     // https://leetcode.com/problems/first-bad-version/?envType=study-plan&id=algorithm-i
     public int firstBadVersion(int n) {
+        int first = 1;
+        int end = n;
+        int mid = 0;
 
-        isBadVersion(3);
-        return 0;
+        while(first < end){
+            mid = first + (end - first)/2;
+
+            if(isBadVersion(mid))
+                end = mid;
+            else
+                first = mid + 1;
+        }
+        return first;
     }
 
 }
--- coding/src/test/java/com/maro/coding/leetcode/FirstBadVersion278Test.java
+++ coding/src/test/java/com/maro/coding/leetcode/FirstBadVersion278Test.java
@@ -7,27 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FirstBadVersion278Test {
     FirstBadVersion278 firstBadVersion = new FirstBadVersion278();
-
     /* 문제에 api가 있는거로 가정하는데 이건 어케 테스트하지..? */
     @Test
     @DisplayName("제공된 예시1")
     void example_1() {
+
         int n = 5;
-        firstBadVersion.setBad(4);
+        int k = firstBadVersion.setBad(n);
 
         int output = firstBadVersion.firstBadVersion(n);
 
-        assertEquals(4, output);
+        System.out.println("k = " + k);
+        assertEquals(k, output);
     }
 
     @Test
     @DisplayName("제공된 예시2")
     void example_2() {
         int n = 1;
-        firstBadVersion.setBad(1);
+        int k = firstBadVersion.setBad(n);
 
         int output = firstBadVersion.firstBadVersion(n);
 
+        System.out.println("k = " + k);
         assertEquals(1, output);
     }
 
@@ -35,23 +37,14 @@ public class FirstBadVersion278Test {
     @DisplayName("테스트 케이스 1 : 엄청 큰 n값")
     void example_3() {
         int n = 100000000;
-        firstBadVersion.setBad(9876543);
+        int k = firstBadVersion.setBad(n);
 
         int output = firstBadVersion.firstBadVersion(n);
 
-        assertEquals(9876543, output);
+        System.out.println("k = " + k);
+        assertEquals(k, output);
     }
 
-    @Test
-    @DisplayName("테스트 케이스 2 : 엄청 큰 n값")
-    void example_4() {
-        int n = 1000000000;
-        firstBadVersion.setBad(987654553);
-
-        int output = firstBadVersion.firstBadVersion(n);
-
-        assertEquals(987654553, output);
-    }
 
 
 }
