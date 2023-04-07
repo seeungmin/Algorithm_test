d46e09d6649184118da1cfbf42cdf97460164a34
Lee <lsmin476@naver.com>
2023-04-07 17:34:13 +0900

    |ADD| Algorithm 283 update 0406

---
+++ coding/src/main/java/com/maro/coding/leetcode/MoveZeroes283.java
@@ -0,0 +1,19 @@
+package com.maro.coding.leetcode;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MoveZeroes283 {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[index++] = nums[i];
+            }
+        }
+
+        for(int j = index; j<nums.length; j++){
+            nums[j] = 0;
+        }
+    }
+}
